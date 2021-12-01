47948c87a63c9aef8a9e5a7ccf043c7fba31f7df
Jesse Caunter <js.caunter@gmail.com>
2021-12-02 08:38:22 +1300

    Add comments to 09_special-pythagorean-triplet.py

--- 09_special-pythagorean-triplet.py
+++ 09_special-pythagorean-triplet.py
@@ -14,6 +14,8 @@ special_pythagorean_triplet = []
 def square_number(x):
     return x ** 2
 
+# We can cut down on uneccessary iterations by calculating half of the number
+# and only looping over numbers in this range.
 def is_pythagorean_triplet(num):
     half = num / 2
     i = 0
@@ -39,10 +41,22 @@ def is_special_triplet(a, b, c):
 def calculate_product(special):
     return special[0] * special[1] * special[2]
 
+# My approach is to iterate over each number in the range, square it, and then add it 
+# to a list of square numbers. Then immediately check whether this number is a 
+# Pythagorean triplet by calculating whether it's the sum of any two preceding
+# numbers in the list. If so, the square root of all three of those numbers get added
+# to a Pythagorean triplet list as a sub-list. We then immediately check whether
+# this Pythagorean triplet is the special one we're looking for.
+# As soon as the special Pythagorean triplet is found, the loop is broken and solution
+# printed on the screen.
+# Integrating all these steps as part of each loop iteration should make algorithm more
+# efficient as it cuts down on total iterations.
 for i in range(1, 999):
     square_num = square_number(i)
     square_num_list.append(square_num)
     is_pythagorean_triplet(square_num)
+    if len(special_pythagorean_triplet) == 3:
+        break
 
 print(f'Special Pythagorean triplet: {special_pythagorean_triplet}')
 print(f'PRODUCT: {calculate_product(special_pythagorean_triplet)}')
